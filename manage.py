2d38a0f46bfa86be81da080b3cd1d5eaf3cf3ea7
Luikipad <btruong2@buffalo.edu>
2023-04-28 13:18:27 -0400

    week11

--- manage.py
+++ manage.py
@@ -1,22 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
 def home():
-    return "Welcome!"
+    return render_template('home.html')
 
-@app.route('/greetings/')
-def greetings():
-    return "How are you doing?"
+# Employee registration page
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 
-@app.route('/greetings/christmas')
-def christmas():
-    return "Merry Christmas!"
-
-@app.route('/greetings/newyear')
-def newyear():
-    return "Happy New Year!"
+# Employee information page
+@app.route('/information')
+def information():
+	con = sqlite3.connect("database.db")
+	con.row_factory = sqlite3.Row
+	cur = con.cursor()
+	cur.execute("select * from students")
+	rows = cur.fetchall();
+	return render_template("list.html",rows = rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
