6d88aed8aa29a41879393f41e02d5e3d9edb6ed6
Luikipad <btruong2@buffalo.edu>
2023-05-05 14:19:58 -0400

    MySQL Addition

--- manage.py
+++ manage.py
@@ -1,27 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, redirect, render_template, request
+from flask_bootstrap import Bootstrap
+
+import mysql.connector as sql
 
 app = Flask(__name__)
+Bootstrap(app)
 
-# Home page
 @app.route('/')
 def home():
-    return render_template('home.html')
+   return render_template('home.htm')
 
-# Employee registration page
-@app.route('/registration')
-def registration():
-    return render_template('registration.html')
+@app.route('/enternew')
+def new_student():
+   return render_template('student.htm')
 
-# Employee information page
-@app.route('/information')
-def information():
-	con = sqlite3.connect("database.db")
-	con.row_factory = sqlite3.Row
-	cur = con.cursor()
-	cur.execute("select * from students")
-	rows = cur.fetchall();
-	return render_template("list.html",rows = rows)
+
+@app.route('/addrec',methods = ['POST', 'GET'])
+def addrec():
+   if request.method == 'POST':
+      try:
+         nm = request.form['nm']
+         addr = request.form['add']
+         city = request.form['city']
+         zip = request.form['zip']
+         
+         with sql.connect(host="localhost", user="flask", password="ubuntu", database="flask_db") as con:
+            cur = con.cursor()
+            cmd = "INSERT INTO students (name,addr,city,zip) VALUES ('{0}','{1}','{2}','{3}')".format(nm,addr,city,zip)
+            cur.execute(cmd)
+            
+            con.commit()
+            msg = "Record successfully added"
+      except:
+         con.rollback()
+         msg = "error in insert operation"
+         
+      finally:
+         return render_template("output.htm",msg = msg)
+         con.close()
+
+@app.route('/list')
+def list():
+   with sql.connect(host="localhost", user="flask", password="ubuntu", database="flask_db") as conn:  
+      cur = conn.cursor()
+      cur.execute("select * from students")
+      rows = cur.fetchall()
+
+   return render_template("list.htm",rows = rows)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+   app.run(debug = True)
---
+++ mysql_create_table.py
@@ -0,0 +1,14 @@
+import mysql.connector as sql
+
+conn = sql.connect(host="localhost", user="flask", password="password", database="flask_db")
+cur = conn.cursor()
+
+cmd = "CREATE TABLE students (\
+    sid INT NOT NULL AUTO_INCREMENT PRIMARY KEY, \
+    name VARCHAR(30) NOT NULL,\
+    addr VARCHAR(30), \
+    city VARCHAR(30), \
+    zip VARCHAR(30))"
+
+cur.execute(cmd)
+conn.close()
